f36861b0842fc019d2f239cef52e15ad721e4d29
Алексей Захаров <zakharov.nnov@gmail.com>
2021-11-01 01:42:58 +0300

    Добавьте файлы проекта.

---
+++ lesson23/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson23
+{
+    //Разработать асинхронный метод для вычисления факториала числа. В методе Main выполнить проверку работы метода.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите целое значение: ");
+            ulong n = Convert.ToUInt64(Console.ReadLine());
+            FactorialAsync(n);
+            Factorial(n);
+            Console.ReadKey();
+
+        }
+        static void Factorial(ulong a)
+        {
+            ulong rezult = 1;
+            for (uint i = 1; i < a + 1; i++)
+            {
+                rezult = rezult * i;
+            }
+            Console.WriteLine("{0}!={1}", a, rezult);
+        }
+        static async Task FactorialAsync(ulong n)
+        {
+            await Task.Run(() => Factorial(n));
+        }
+    }
+}
